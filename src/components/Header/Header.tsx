1626283a978c510074828202e06c72f3ecf4af4d
Adam Stępień <vesemir2k@gmail.com>
2024-06-19 19:57:11 +0200

    📝 (Header): update CSS class name from .imageHero to .header__image-hero for consistency and clarity ♻️ (Header): refactor Header component to destructure styles object for better readability and maintainability

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,12 +3,16 @@ import styles from './Header.module.css';
 import Heading from './Heading';
 import TopBar from './TopBar';
 
-const Header = () => (
-  <header className={styles.header}>
-    <TopBar />
-    <Heading />
-    <img className={styles.imageHero} src={imageHero} alt="" />
-  </header>
-);
+const Header = () => {
+  const { header: headerClass, 'header__image-hero': imageHeroClass } = styles;
+
+  return (
+    <header className={headerClass}>
+      <TopBar />
+      <Heading />
+      <img className={imageHeroClass} src={imageHero} alt="" />
+    </header>
+  );
+};
 
 export default Header;
