2efb50c2935c17475cd8fca4e6f175b5de6e638f
Adam Stępień <vesemir2k@gmail.com>
2024-06-19 14:41:52 +0200

    ✨ (App.tsx): Add ProductBenefits component to the main App component to display product benefits section 📝 (Header.module.css): Remove unnecessary border-bottom style from the Header component ✨ (ProductBenefits): Add ProductBenefits component with corresponding CSS module for styling 📝 (index.tsx): Export ProductBenefits component from index file for easier importing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import ProductBenefits from './components/ProductBenefits';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <ProductBenefits />
     </div>
   );
 }
---
+++ src/components/ProductBenefits/ProductBenefits.tsx
@@ -0,0 +1,7 @@
+import styles from './ProductBenefits.module.css';
+
+const ProductBenefits = () => {
+  return <div className={styles.productBenefits}>dsadas</div>;
+};
+
+export default ProductBenefits;
