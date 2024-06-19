93fac6cac2adad63965eb226a7c56a1b8b6491be
Adam Stępień <vesemir2k@gmail.com>
2024-06-19 19:59:12 +0200

    📝 (TopBar): update CSS class name from .topBar to .top-bar for consistency and readability ♻️ (TopBar): refactor TopBar component to destructure CSS class name for better code organization and maintainability

--- src/components/Header/TopBar/TopBar.tsx
+++ src/components/Header/TopBar/TopBar.tsx
@@ -1,13 +1,16 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import logo from '../../../assets/logo-light.svg';
 import Button from '../../Button';
 import styles from './TopBar.module.css';
 
-const TopBar = () => (
-  <div className={styles.topBar}>
-    <img src={logo} alt="logo" />
-    <Button variant="underline" text="Apply for access" />
-  </div>
-);
+const TopBar = () => {
+  const { 'top-bar': topBarClass } = styles;
+
+  return (
+    <div className={topBarClass}>
+      <img src={logo} alt="logo" />
+      <Button variant="underline" text="Apply for access" />
+    </div>
+  );
+};
 
 export default TopBar;
