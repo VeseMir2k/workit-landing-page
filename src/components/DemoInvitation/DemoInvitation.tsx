443781813d768e1dc764ad8f3ef1c0e6a594cc97
Adam Stępień <vesemir2k@gmail.com>
2024-06-20 22:39:39 +0200

    📝 (DemoInvitation): update CSS class names to use kebab-case for better readability and consistency 📝 (DemoInvitation): add new CSS class for image founder to improve styling and maintainability of the component

--- src/components/DemoInvitation/DemoInvitation.tsx
+++ src/components/DemoInvitation/DemoInvitation.tsx
@@ -1,5 +1,14 @@
+import imageFounder from '../../assets/image-founder.webp';
 import styles from './DemoInvitation.module.css';
 
-const DemoInvitation = () => <div className={styles.demoInvitation}></div>;
+const DemoInvitation = () => {
+  const { 'demo-invitation': demoInvitationClass, 'demo-invitation__image-founder': imageFounderClass } = styles;
+
+  return (
+    <div className={demoInvitationClass}>
+      <img className={imageFounderClass} src={imageFounder} alt="image founder" />
+    </div>
+  );
+};
 
 export default DemoInvitation;
