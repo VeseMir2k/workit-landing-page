86d76f4c357b507d4f3a9a612a48e254a683f1ec
Adam Stępień <vesemir2k@gmail.com>
2024-06-25 15:53:16 +0200

    📝 (DemoInvitation): Update DemoInvitation styles to improve layout and readability 📝 (DemoInvitation): Refactor DemoInvitation component to use new styles and add a Button component

--- src/components/DemoInvitation/DemoInvitation.tsx
+++ src/components/DemoInvitation/DemoInvitation.tsx
@@ -1,17 +1,25 @@
+import Button from '../Button';
 import styles from './DemoInvitation.module.css';
 
 const DemoInvitation = () => {
-  const { 'demo-invitation': demoInvitationClass, 'demo-invitation__image-founder': imageFounderClass } = styles;
+  const {
+    'demo-invitation': demoInvitationClass,
+    'demo-invitation__image-founder': imageFounderClass,
+    'demo-invitation__content': contentClass,
+    'demo-invitation__title': titleClass,
+    'demo-invitation__description': descriptionClass,
+  } = styles;
 
   return (
     <div className={demoInvitationClass}>
       {/* <img className={imageFounderClass} src={imageFounder} alt="image founder" /> */}
-      <div>
-        <h2>Be the first to test</h2>
-        <p>
+      <div className={contentClass}>
+        <h2 className={titleClass}>Be the first to test</h2>
+        <p className={descriptionClass}>
           Hi, I&apos;m Louis Graham, the founder of the company. Book a demo call with me to become a beta tester for
           our app and kickstart your company. Apply for access below and I`ll be in touch to schedule a call.
         </p>
+        <Button variant="full" text="Apply for access" />
       </div>
     </div>
   );
