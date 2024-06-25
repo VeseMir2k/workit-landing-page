4c9f8e53f9dda2de4b423e25cb4261733878e5ab
Adam Stępień <vesemir2k@gmail.com>
2024-06-25 15:23:44 +0200

    📝 (Button.module.css): Add hover styles for button--underline and button--full classes to improve user experience 📝 (DemoInvitation.tsx): Comment out unused imageFounder import and replace it with text content to improve component readability

--- src/components/DemoInvitation/DemoInvitation.tsx
+++ src/components/DemoInvitation/DemoInvitation.tsx
@@ -1,4 +1,3 @@
-import imageFounder from '../../assets/image-founder.webp';
 import styles from './DemoInvitation.module.css';
 
 const DemoInvitation = () => {
@@ -6,7 +5,14 @@ const DemoInvitation = () => {
 
   return (
     <div className={demoInvitationClass}>
-      <img className={imageFounderClass} src={imageFounder} alt="image founder" />
+      {/* <img className={imageFounderClass} src={imageFounder} alt="image founder" /> */}
+      <div>
+        <h2>Be the first to test</h2>
+        <p>
+          Hi, I&apos;m Louis Graham, the founder of the company. Book a demo call with me to become a beta tester for
+          our app and kickstart your company. Apply for access below and I`ll be in touch to schedule a call.
+        </p>
+      </div>
     </div>
   );
 };
